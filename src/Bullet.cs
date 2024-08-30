8c653ae6c52022f2e3f96e4a2bb460dbe809dc47
Skyfrei <tarkaklavio@gmail.com>
2024-08-30 16:42:10 +0200

    changes

--- src/Bullet.cs
+++ src/Bullet.cs
@@ -8,14 +8,28 @@ public partial class Bullet : RigidBody3D{
     
     private BulletType type;
     private bool flying = false;
+    private Vector3 flyingDirection;
+    private float bulletSpd = 10f;
 
-    private CharacterBody3D origin;
     private RigidBody3D bullet;
     private CollisionShape3D bulletShape;
     private MeshInstance3D bulletMesh;
+    private CharacterBody3D shooter;
 
-    public Bullet(CharacterBody3D o){
-        origin = o;
+    public Bullet(ref CharacterBody3D o){
+        GravityScale = 0.0f;
+
+        shooter = o;
+        SetScript(ResourceLoader.Load("res://src/Bullet.cs"));
+        this.Position = shooter.Position;
+        bulletShape = new CollisionShape3D();
+        bulletMesh = new MeshInstance3D();
+        this.AddChild(bulletShape);
+        bulletMesh.Mesh = ((Mesh)ResourceLoader.Load("res://assets/bullet.res"));
+        bulletShape.AddChild(bulletMesh);
+        ContactMonitor = true; 
+        MaxContactsReported = 2;
+        
     }
     public  void _init(CharacterBody3D o){
     }
@@ -25,30 +39,35 @@ public partial class Bullet : RigidBody3D{
     }
 
     public override void _PhysicsProcess(double delta){
-
+        //CheckCollision
+        if (GetCollidingBodies().Count > 0){
+            flying = false;
+            GD.Print("A");
+        }
+        Fly(delta);
+        //unload meshes
     }
-
+    
+    public void StartFlying(Vector3 fldir){
+        if (!IsFlying()){
+            this.Position = shooter.Position;
+            flyingDirection = fldir;
+            flying = true;
+        }
+    }
     public bool IsFlying(){
         return flying;
     }
 
-    public void Fly(){
-        InitBulletGraphics();
-        flying = true;
-    }
+    private void Fly(double delta){
+        Vector3 newV = new Vector3(this.Position.X + flyingDirection.X,
+               this.Position.Y + flyingDirection.Y,
+               this.Position.Z + flyingDirection.Z) * (float)delta * bulletSpd;
 
-    private void InitBulletGraphics(){
-        if (bullet == null){
-            
-        }
-        if (bulletShape == null){
-
-        }
-
-        if (bulletMesh == null){
-
-        }
-
+        this.Position = newV;
+        bulletMesh.Position = newV;
+        bulletShape.Position = newV;
+        GD.Print(bulletMesh.Position);
     }
 
     private void ProcBulletEffect(){
--- src/Player.cs
+++ src/Player.cs
@@ -5,70 +5,73 @@ using System.Collections.Generic;
 public partial class Player : CharacterBody3D{
 	private List<Skill> pow; 
 	private List<Bullet> bullets;
-    private float health;
+	private float health;
 	private float critChance;
 	private float critMult;
 	private float atkspd;
 	private ushort level = 1;
-    private float movspeed = 5.0f;
-    private int bulletNumber = 30;
-    private CharacterBody3D p;
+	private float movspeed = 5.0f;
+	private int bulletNumber = 1;
+	private CharacterBody3D p;
 
 	public override void _Ready(){
 		atkspd = 1.0f;
 		health = 100.0f;
 		critChance = 0.05f;
 		critMult = 1.15f;
-        p = GetParent().GetNode<CharacterBody3D>("Player");
-        for (int i = 0; i < bulletNumber; i++){
-//            bullets.Add(new Bullet(pl));
-        }
+		bullets = new List<Bullet>();
+		p = GetParent().GetNode<CharacterBody3D>("Player");
+
+		for (int i = 0; i < bulletNumber; i++){
+			Bullet b = new Bullet(ref p);
+            p.AddChild(b);
+            bullets.Add(b);
+		}
 	}
 
 	public override void _PhysicsProcess(double delta){
 
-        TakeInput(delta);
+		TakeInput(delta);
+	}
 
-    }
 
+	private void TakeInput(double delta){
+		var direction = Vector3.Zero;
+		if (Input.IsActionPressed("space")){
+			direction.Y += 1;
+		}
+		if (Input.IsActionPressed("right")){
+			direction.X -= 1;
+		}
+		if (Input.IsActionPressed("left")){
+			direction.X += 1;
+		}
+		if (Input.IsActionPressed("up")){
+			direction.Z += 1;
+		}
+		if (Input.IsActionPressed("down")){
+			direction.Z -= 1;
+		}
+		if (direction != Vector3.Zero){
+			direction = direction.Normalized();
+		}
+		//Implement being clicked only once
+		if (Input.IsActionPressed("q")){
+			ShootBullet(direction);
+		}
+		p.Rotation = new Vector3(direction.X, direction.Y, direction.Z);
+		MoveAndCollide((float)delta * direction * movspeed);
+	}
 
-    private void TakeInput(double delta){
-        var direction = Vector3.Zero;
-        if (Input.IsActionPressed("space")){
-            direction.Y += 1;
-        }
-        if (Input.IsActionPressed("right")){
-            direction.X -= 1;
-        }
-        if (Input.IsActionPressed("left")){
-            direction.X += 1;
-        }
-        if (Input.IsActionPressed("up")){
-            direction.Z += 1;
-        }
-        if (Input.IsActionPressed("down")){
-            direction.Z -= 1;
-        }
-        if (direction != Vector3.Zero){
-            direction = direction.Normalized();
-        }
-        //Implement being clicked only once
-        if (Input.IsActionPressed("q")){
-            ShootBullet();
-        }
-        p.Rotation = new Vector3(direction.X, direction.Y, direction.Z);
-        MoveAndCollide((float)delta * direction * movspeed);
-    }
+	private void ShootBullet(Vector3 flyingDir){
+	  foreach (var bullet in bullets){
+		if (!bullet.IsFlying()){
+			bullet.StartFlying(flyingDir);
+		}
+	  }
+	}
 
-    private void ShootBullet(){
-      foreach (var bullet in bullets){
-        if (!bullet.IsFlying()){
-            bullet.Fly();
-        }
-      }
-    }
-
-    
+	
 
 	public void AddCharacterSkill(Skill s){
 		pow.Add(s);
