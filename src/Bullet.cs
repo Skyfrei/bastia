a893a5c8b9e506c032d3dedd782326419af0df4b
Skyfrei <tarkaklavio@gmail.com>
2024-08-31 17:45:50 +0200

    as

--- src/Bullet.cs
+++ src/Bullet.cs
@@ -1,68 +1,75 @@
 using Godot;
 
 enum BulletType{
-    NORMAL,
+	NORMAL,
 };
 
 public partial class Bullet : RigidBody3D{
-    
-    private BulletType type;
-    private bool flying = false;
-    private Vector3 flyingDirection;
-    private float bulletSpd = 1.0f;
+	
+	private BulletType type;
+	private bool flying = false;
+	private Vector3 flyingDirection;
+	private float bulletSpd = 1.2f;
+	private float scaleFactor = 0.1f;
 
-    private RigidBody3D bullet;
-    private CollisionShape3D bulletShape;
-    private MeshInstance3D bulletMesh;
-    private CharacterBody3D shooter;
+	private RigidBody3D bullet;
+	private CollisionShape3D bulletShape;
+	private MeshInstance3D bulletMesh;
+	private CharacterBody3D shooter;
 
-    public Bullet(){
-        GravityScale = 0.0f;
+	public Bullet(){
+		GravityScale = 0.0f;
+		Visible = false;
 
-        SetScript(ResourceLoader.Load("res://src/Bullet.cs"));
-        bulletShape = new CollisionShape3D();
-        bulletMesh = new MeshInstance3D();
-        this.AddChild(bulletShape);
-        bulletMesh.Mesh = ((Mesh)ResourceLoader.Load("res://assets/bullet.res"));
-        bulletShape.AddChild(bulletMesh);
-        ContactMonitor = true; 
-        MaxContactsReported = 2;
-        
-    }
-    public override void _Ready(){
 
-    }
+		SetScript(ResourceLoader.Load("res://src/Bullet.cs"));
+		bulletShape = new CollisionShape3D();
+		bulletShape.Scale *= scaleFactor;
+		bulletMesh = new MeshInstance3D();
+		this.AddChild(bulletShape);
+		bulletMesh.Mesh = ((Mesh)ResourceLoader.Load("res://assets/bullet.res"));
+		bulletShape.AddChild(bulletMesh);
+		ContactMonitor = true; 
+		MaxContactsReported = 2;
 
-    public override void _PhysicsProcess(double delta){
-        //CheckCollision
-        if (GetCollidingBodies().Count > 0){
-            flying = false;
-            GD.Print("collided");
-        }
-        if (IsFlying())
-            Fly(delta);
-        //unload meshes
-    }
-    
-    public void StartFlying(Vector3 fldir, Vector3 shooter){
-        if (!IsFlying()){
-            Position = shooter;
-            flyingDirection = fldir;
-            flying = true;
-        }
-    }
-    public bool IsFlying(){
-        return flying;
-    }
+		
+	}
+	public override void _Ready(){
 
-    private void Fly(double delta){
-        Position = Position + flyingDirection * (float)delta * bulletSpd;
-        bulletMesh.Position = Position;
-        bulletShape.Position = Position;
-    }
+	}
 
-    private void ProcBulletEffect(){
+	public override void _PhysicsProcess(double delta){
+		//CheckCollision
+		if (GetCollidingBodies().Count > 0){
+			flying = false;
+			Visible = false;
+			GD.Print("collided");
+		}
+		if (IsFlying())
+			Fly(delta);
+		//unload meshes
+	}
+	
+	public void StartFlying(Vector3 fldir, Vector3 shooter){
+		if (!IsFlying()){
+			Position = shooter;
+			flyingDirection = fldir;
+			flying = true;
+			Visible = true;
+		}
+	}
+	public bool IsFlying(){
+		return flying;
+	}
 
-    }
+	private void Fly(double delta){
+		Position = Position + flyingDirection * (float)delta * bulletSpd;
+		bulletMesh.Position = Position;
+		bulletShape.Position = Position;
+	}
+
+	private void ProcBulletEffect(){
+
+	}
 
 }
